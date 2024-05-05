fd5bf5236c43a659ebbaa8fb71b6cf3bac3a63fc
La-Shallot <shallotwang129@gmail.com>
2024-05-05 03:57:20 -0400

    corner working???

--- src/LkFlow.cpp
+++ src/LkFlow.cpp
@@ -13,6 +13,7 @@ cv::Mat Lk_Flow::CalcLkFlow(cv::Mat imgA, cv::Mat imgB, int xsarea, int ysarea, 
         cv::Mat outimg = imgB.clone();
 
         int dimx = imgA.cols, dimy = imgA.rows;
+        printf("\nDimx: %d, Dimy: %d", dimx, dimy);
 
         // iterate through each corner to find flow vectors
 
@@ -69,6 +70,13 @@ cv::Mat Lk_Flow::CalcLkFlow(cv::Mat imgA, cv::Mat imgB, int xsarea, int ysarea, 
 
                     double curdI = ((int)imgA.at<uchar>(y, x) - (int)imgB.at<uchar>(y, x));
 
+                    // if (i == 0) {
+                    //     printf("\nLLLLLLLCorner: (%d, %d)---------------------------------------------------------------", x, y);
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - Ix: %f, Iy: %f", cur_corner.x, cur_corner.y, curIx, curIy);
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - x: %d, y: %d - px: %d, nx: %d, py: %d, ny: %d", cur_corner.x, cur_corner.y, x, y, px, nx, py, ny);
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - imgA[y][px]: %ud, imgA[y][nx]: %ud, imgA[py][x]: %ud, imgA[ny][x]: %ud", cur_corner.x, cur_corner.y, imgA.at<uchar>(y, px), imgA.at<uchar>(y, nx), imgA.at<uchar>(py, x), imgA.at<uchar>(ny, x));
+                    // }
+
                     b[0] += curdI * curIx;
                     b[1] += curdI * curIy;
                 }
@@ -81,7 +89,7 @@ cv::Mat Lk_Flow::CalcLkFlow(cv::Mat imgA, cv::Mat imgB, int xsarea, int ysarea, 
             Ginv[1][1] = G[0][0] / detG;
 
             double V[2] = {Ginv[0][0] * b[0] + Ginv[0][1] * b[1], Ginv[1][0] * b[0] + Ginv[1][1] * b[1]};
-            if (verbose)
+            if (verbose or i < 100)
                 printf("\nFor the corner (%d,%d) - v = (%f,%f)", cur_corner.x, cur_corner.y, V[0], V[1]);
             cv::Scalar color = (0, 255, 0); 
 
@@ -106,7 +114,7 @@ cv::Mat Lk_Flow::CalcLkFlow(cv::Mat imgA, cv::Mat imgB, int xsarea, int ysarea, 
         return outimg;
     }
 
-std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector<float>>& imgA, std::vector<std::vector<float>>& imgB, int xsarea, int ysarea, int xarea, int yarea, std::vector<std::pair<float, float>> corners, bool verbose)
+std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector<float>>& imgA, std::vector<std::vector<float>>& imgB, int xsarea, int ysarea, int xarea, int yarea, std::vector<std::pair<int, int>> corners, bool verbose)
     {
         // start timer
         
@@ -115,7 +123,7 @@ std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector
         // int dimx = imgA.cols, dimy = imgA.rows;
         int dimx = imgA[0].size(), dimy = imgA.size();
 
-        std::vector<std::pair<float, float>> out;
+        std::vector<std::pair<float, float>> out(corners.size());
 
         // iterate through each corner to find flow vectors
 
@@ -164,8 +172,15 @@ std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector
                     if (y >= (dimy - 1))
                         ny = y;
 
-                    double curIx = ((int)imgA[y][px] - (int)imgA[y][nx]) / 2;
-                    double curIy = ((int)imgA[px][y] - (int)imgA[ny][x]) / 2;
+                    double curIx = (imgA[y][px] - imgA[y][nx]) / 2;
+                    // printf("\ncurIx: %f, imgA[y][px]: %f, imgA[y][nx]: %f", curIx, imgA[y][px], imgA[y][nx]); 
+                    double curIy = (imgA[py][x] - imgA[ny][x]) / 2;
+
+                    // if (i == 0) {
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - Ix: %f, Iy: %f", cur_corner.first, cur_corner.second, curIx, curIy);
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - x: %d, y: %d - px: %d, nx: %d, py: %d, ny: %d", cur_corner.first, cur_corner.second, x, y, px, nx, py, ny);
+                    //     printf("\nLLLLLLLCorner: (%d, %d) - imgA[y][px]: %f, imgA[y][nx]: %f, imgA[py][x]: %f, imgA[ny][x]: %f", cur_corner.first, cur_corner.second, imgA[y][px], imgA[y][nx], imgA[py][x], imgA[ny][x]);
+                    // }
 
                     // calculate G and b
                     G[0][0] += curIx * curIx;
@@ -173,7 +188,7 @@ std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector
                     G[1][0] += curIx * curIy;
                     G[1][1] += curIy * curIy;
 
-                    double curdI = ((int)imgA[y][x] - (int)imgB[y][x]);
+                    double curdI = (imgA[y][x] - imgB[y][x]);
 
                     b[0] += curdI * curIx;
                     b[1] += curdI * curIy;
@@ -188,8 +203,11 @@ std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector
 
             double V[2] = {Ginv[0][0] * b[0] + Ginv[0][1] * b[1], Ginv[1][0] * b[0] + Ginv[1][1] * b[1]};
 
+            // if (i < 100) {
+            //     printf("\nFor the corner (%d,%d) - v = (%f,%f)", cur_corner.first, cur_corner.second, V[0], V[1]);
+            // }
             #pragma omp critical
-            out.push_back(std::make_pair(V[0], V[1]));
+            out[i] = std::make_pair(V[0], V[1]);
             // if (verbose)
                 // printf("\nFor the corner (%d,%d) - v = (%f,%f)", cur_corner.x, cur_corner.y, V[0], V[1]);
 
@@ -210,7 +228,165 @@ std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow(std::vector<std::vector
         ///    cout << string("\nWriting lkoutput to o") << string(filename);
         //    imwrite(string("o") + string(filename), outimg);
         //}
+
+        #pragma omp barrier
         
         return out;
     }
+
+    // std::vector<std::pair<float, float>> Lk_Flow::CalcLkFlow2(std::vector<std::vector<float>>& imgA, std::vector<std::vector<float>>& imgB, int xsarea, int ysarea, int xarea, int yarea, std::vector<std::pair<int, int>> corners, bool verbose)
+    // {
+    //     // start timer
+        
+    //     // cv::Mat outimg = imgB.clone();
+
+    //     // int dimx = imgA.cols, dimy = imgA.rows;
+    //     int dimx = imgA[0].size(), dimy = imgA.size();
+
+    //     std::vector<std::pair<float, float>> out(corners.size());
+
+    //     // iterate through each corner to find flow vectors
+
+    //     #pragma omp parallel for schedule(static, 32)
+    //     for (int i = 0; i < corners.size(); i++)
+    //     {
+    //         auto cur_corner = corners[i];
+    //         auto corner_mid = std::make_pair((int)(cur_corner.first + (int)(xarea / 2)), (int)(cur_corner.second + (int)(yarea / 2)));
+
+    //         // Draw the corner in the out-image
+    //         // #pragma omp critical
+    //         // cv::rectangle(outimg, cur_corner, cur_corner + cv::Point(xarea, yarea), cv::Scalar(0), 2);
+
+    //         // Set range parameters
+    //         // cv::Range sry = cv::Range(std::max(0, corner_mid.y - (int)(ysarea / 2)), std::min(dimy-1, corner_mid.y + (int)(ysarea / 2)));
+    //         // cv::Range srx = cv::Range(std::max(0, corner_mid.x - (int)(xsarea / 2)), std::min(dimx-1, corner_mid.x + (int)(xsarea / 2)));
+
+    //         auto sry = std::make_pair(std::max(0, corner_mid.second - (int)(ysarea / 2)), std::min(dimy-1, corner_mid.second + (int)(ysarea / 2)));
+    //         auto srx = std::make_pair(std::max(0, corner_mid.first - (int)(xsarea / 2)), std::min(dimx-1, corner_mid.first + (int)(xsarea / 2)));
+
+    //         // Now that we've found search windows, we can proceed to calculate Ix and Iy for each pixel in the search window
+    //         // Ix
+
+    //         int range = 1;
+    //         // gradient cv::Matrix
+    //         double G[2][2] = {0, 0, 0, 0};
+
+    //         // IxIt and IyIt
+    //         double b[2] = {0, 0};
+
+    //         // This is a convolution?
+
+    //         double G_00 = 0, G_01 = 0, G_10 = 0, G_11 = 0;
+    //         double b_0 = 0, b_1 = 0;
+
+    //         int sr_total = (srx.second - srx.first + 1) * (sry.second - sry.first + 1);
+    //         for (int xy = 0; xy < sr_total; xy++)
+    //         {
+    //             int x = srx.first + xy % (srx.second - srx.first + 1);
+    //             int y = sry.first + xy / (srx.second - srx.first + 1);
+
+    //             int px = x - range, nx = x + range;
+    //             int py = y - range, ny = y + range;
+
+    //             // edge conditions
+    //             if (x == 0)
+    //                 px = x;
+    //             if (x >= (dimx - 1))
+    //                 nx = x;
+    //             if (y == 0)
+    //                 py = 0;
+    //             if (y >= (dimy - 1))
+    //                 ny = y;
+
+    //             double curIx = (imgA[y][px] - imgA[y][nx]) / 2;
+    //             double curIy = (imgA[py][x] - imgA[ny][x]) / 2;
+
+    //             // calculate G and b
+    //             G_00 += curIx * curIx;
+    //             G_01 += curIx * curIy;
+    //             G_10 += curIx * curIy;
+    //             G_11 += curIy * curIy;
+
+    //             double curdI = (imgA[y][x] - imgB[y][x]);
+
+    //             b_0 += curdI * curIx;
+    //             b_1 += curdI * curIy;
+    //         }
+    //         // for (int x = srx.first; x <= srx.second; x++)
+    //         //     for (int y = sry.first; y <= sry.second; y++)
+    //         //     {
+    //         //         int px = x - range, nx = x + range;
+    //         //         int py = y - range, ny = y + range;
+
+    //         //         // edge conditions
+    //         //         if (x == 0)
+    //         //             px = x;
+    //         //         if (x >= (dimx - 1))
+    //         //             nx = x;
+    //         //         if (y == 0)
+    //         //             py = 0;
+    //         //         if (y >= (dimy - 1))
+    //         //             ny = y;
+
+    //         //         double curIx = (imgA[y][px] - imgA[y][nx]) / 2;
+    //         //         // printf("\ncurIx: %f, imgA[y][px]: %f, imgA[y][nx]: %f", curIx, imgA[y][px], imgA[y][nx]); 
+    //         //         double curIy = (imgA[py][x] - imgA[ny][x]) / 2;
+
+    //         //         // if (i == 0) {
+    //         //         //     printf("\nLLLLLLLCorner: (%d, %d) - Ix: %f, Iy: %f", cur_corner.first, cur_corner.second, curIx, curIy);
+    //         //         //     printf("\nLLLLLLLCorner: (%d, %d) - x: %d, y: %d - px: %d, nx: %d, py: %d, ny: %d", cur_corner.first, cur_corner.second, x, y, px, nx, py, ny);
+    //         //         //     printf("\nLLLLLLLCorner: (%d, %d) - imgA[y][px]: %f, imgA[y][nx]: %f, imgA[py][x]: %f, imgA[ny][x]: %f", cur_corner.first, cur_corner.second, imgA[y][px], imgA[y][nx], imgA[py][x], imgA[ny][x]);
+    //         //         // }
+
+    //         //         // calculate G and b
+    //         //         G[0][0] += curIx * curIx;
+    //         //         G[0][1] += curIx * curIy;
+    //         //         G[1][0] += curIx * curIy;
+    //         //         G[1][1] += curIy * curIy;
+
+    //         //         double curdI = (imgA[y][x] - imgB[y][x]);
+
+    //         //         b[0] += curdI * curIx;
+    //         //         b[1] += curdI * curIy;
+    //         //     }
+
+    //         double detG = (G[0][0] * G[1][1]) - (G[0][1] * G[1][0]);
+    //         double Ginv[2][2] = {0, 0, 0, 0};
+    //         Ginv[0][0] = G[1][1] / detG;
+    //         Ginv[0][1] = -G[0][1] / detG;
+    //         Ginv[1][0] = -G[1][0] / detG;
+    //         Ginv[1][1] = G[0][0] / detG;
+
+    //         double V[2] = {Ginv[0][0] * b[0] + Ginv[0][1] * b[1], Ginv[1][0] * b[0] + Ginv[1][1] * b[1]};
+
+    //         // if (i < 100) {
+    //         //     printf("\nFor the corner (%d,%d) - v = (%f,%f)", cur_corner.first, cur_corner.second, V[0], V[1]);
+    //         // }
+    //         #pragma omp critical
+    //         out[i] = std::make_pair(V[0], V[1]);
+    //         // if (verbose)
+    //             // printf("\nFor the corner (%d,%d) - v = (%f,%f)", cur_corner.x, cur_corner.y, V[0], V[1]);
+
+
+
+    //         // #pragma omp critical
+    //         // cv::line(outimg, corner_mid, corner_mid + cv::Point(V[0] * 10, V[1] * 10), color, 1);
+    //     }
+    //     // visualization: draw the flow vectors
+
+    //     // string winCorImg = "Corners found";
+    //     // namedWindow(winCorImg, WINDOW_NORMAL);
+    //     // imshow(winCorImg, outimg);
+    //     // waitKey(0);
+
+    //     //if (filename != NULL)
+    //     //{
+    //     ///    cout << string("\nWriting lkoutput to o") << string(filename);
+    //     //    imwrite(string("o") + string(filename), outimg);
+    //     //}
+
+    //     #pragma omp barrier
+        
+    //     return out;
+    // }
     
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <getopt.h>
 
 #include <morevac_corner.h>
+#include <harris_corner.h>
 #include <lk_flow.h>
     
 using namespace cv;
@@ -19,24 +20,30 @@ using namespace std;
 // mat to vectors
 std::vector<std::vector<float>> mat2vec(Mat img)
 {
+    // printf("\nMat size: %d, %d", img.cols, img.rows);
     std::vector<std::vector<float>> out;
-    for (int i = 0; i < img.cols; i++)
+    for (int y = 0; y < img.rows; y++)
     {
         std::vector<float> col;
-        for (int j = 0; j < img.rows; j++)
+        for (int x = 0; x < img.cols; x++)
         {
-            float val = ((int)img.at<uchar>(j, i))/255.0;
+            float val = (float)((int)img.at<uchar>(y, x));
+            if (val < 0) {
+                printf("\nNegative value: %f", val);
+            }
             col.push_back(val);
         }
         out.push_back(col);
     }
+    // printf("\nVector size: %d, %d", out.size(), out[0].size());
+    //print part of the vector
     return out;
 }
 
 
 int main(int argc, char *argv[]) {
 
-    int num_threads = 0;
+    int num_threads = 1;
     int opt;
     while ((opt = getopt(argc, argv, "f:n:p:i:m:b:")) != -1) {
         switch (opt) {
@@ -49,7 +56,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    std::cout << "Number of threads: " << num_threads << '\n';\
+    std::cout << "Number of threads: " << num_threads << "-----------------------------------------------------------------------\n";
 
     if (num_threads > 0) {
         omp_set_num_threads(num_threads);
@@ -58,10 +65,10 @@ int main(int argc, char *argv[]) {
     int num_frames = 15;
 
     // corner finding params
-    int xarea = 3, yarea = 3, thres = 1;
+    int xarea = 5, yarea = 5, thres = 2000;
 
     // LK params
-    int lk_xarea = 40, lk_yarea = 40;
+    int lk_xarea = 50, lk_yarea = 50;
 
     int sumCornerTime = 0;
     int sumLKTime = 0;
@@ -72,7 +79,7 @@ int main(int argc, char *argv[]) {
     auto curimg = Mat();
 
     //    Step 2 - Implementing Lucas-Kanade Tracker
-    for (int i = 1; i < num_frames; i++)
+    for (int i = 30; i < 30 + num_frames; i++)
     {
         char buffer[50];
 
@@ -92,43 +99,69 @@ int main(int argc, char *argv[]) {
 
         // Compute Time :D
 
-        deque<Point> corn = MorevacCorner::findCorners(previmg, xarea, yarea, thres, false, false);
+        // deque<Point> corn = MorevacCorner::findCorners(previmg, xarea, yarea, thres, false, false);
+        vector<pair<int, int>> corn_vec = MorevacCorner::findCorners(previmgvec, xarea, yarea, thres, false);
         const auto endcorner = chrono::steady_clock::now();
 
-        vector<pair<float, float>> corn_vec_not_used = MorevacCorner::findCorners(previmgvec, xarea, yarea, thres, false);
-
-        const auto endcorner2 = chrono::steady_clock::now();
-
-        Lk_Flow::CalcLkFlow(previmg, curimg, lk_xarea, lk_yarea, xarea, yarea, corn, false);
+        // sort corners by y axis and x axis
+        // sort(corn_vec.begin(), corn_vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        //     if (a.first == b.first) {
+        //         return a.second < b.second;
+        //     }
+        //     return a.first < b.first;
+        // });
+        // Lk_Flow::CalcLkFlow(previmg, curimg, lk_xarea, lk_yarea, xarea, yarea, corn, false);
+        auto lk_flow_res = Lk_Flow::CalcLkFlow(previmgvec, curimgvec, lk_xarea, lk_yarea, xarea, yarea, corn_vec, false);
         const auto endlk = chrono::steady_clock::now();
 
-        printf("\nInit took %d ms, Corner detection took %d ms, Lucas-Kanade took %d ms", chrono::duration_cast<chrono::milliseconds>(endinit - begin).count(), chrono::duration_cast<chrono::milliseconds>(endcorner - endinit).count(), chrono::duration_cast<chrono::milliseconds>(endlk - endcorner).count());
+        // printf("\nInit took %ld ms, Corner detection took %ld ms, Lucas-Kanade took %ld ms", chrono::duration_cast<chrono::milliseconds>(endinit - begin).count(), chrono::duration_cast<chrono::milliseconds>(endcorner - endinit).count(), chrono::duration_cast<chrono::milliseconds>(endlk - endcorner).count());
+        
+        vector<pair<int, int>> corn_vec2 = HarrisCorner::findCorners(previmgvec, xarea, yarea, 1000000000, false);
+        const auto endcorner2 = chrono::steady_clock::now();
 
-        // transform vector point to vec pair
-        std::vector<std::pair<float, float>> corn_vec(corn.size());
-        for (int i = 0; i < corn.size(); i++)
-        {
-            corn_vec[i] = std::make_pair(corn[i].x, corn[i].y);
-        }
-        Lk_Flow::CalcLkFlow(previmgvec, curimgvec, lk_xarea, lk_yarea, xarea, yarea, corn_vec, false);
+        printf("\n>>>Found %d corners", corn_vec2.size());
 
+        const auto lk_flow_res2 = Lk_Flow::CalcLkFlow(previmgvec, curimgvec, lk_xarea, lk_yarea, xarea, yarea, corn_vec2, false);
         const auto endlk2 = chrono::steady_clock::now();
 
         // visual stuff
+        std::vector<Point> corn;
+        for (int i = 0; i < corn_vec2.size(); i++)
+        {
+            corn.push_back(Point(corn_vec2[i].first, corn_vec2[i].second));
+        }
+
+        Mat visual_corners = previmg.clone();
+        for (int i = 0; i < corn.size(); i++)
+        {
+            circle(visual_corners, corn[i], 100, Scalar(0, 255, 0), 2);
+        }
+
+        Mat visual_lk = previmg.clone();
+        for (int i = 0; i < lk_flow_res2.size(); i++)
+        {   
+            // if (i < 100) {
+            //     printf("\nCorner: (%d, %d) - Flow: (%f, %f)", corn[i].x, corn[i].y, lk_flow_res[i].first, lk_flow_res[i].second);
+            // }
+            circle(visual_lk, corn[i], 3, Scalar(0, 255, 0), 2);
+            line(visual_lk, corn[i], Point(corn[i].x + lk_flow_res2[i].first*10, corn[i].y + lk_flow_res2[i].second*10), Scalar(0, 255, 0), 1);
+        }
+
+        namedWindow("lk", WINDOW_NORMAL);
+        setWindowProperty("lk", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+        imshow("lk", visual_lk);
+
+        waitKey(0);
 
         const auto endvisual = chrono::steady_clock::now();
-
-        printf("\nInit took %d ms, Corner detection took %d ms, Lucas-Kanade took %d ms", chrono::duration_cast<chrono::milliseconds>(endinit - begin).count(), chrono::duration_cast<chrono::milliseconds>(endcorner - endinit).count(), chrono::duration_cast<chrono::milliseconds>(endlk - endcorner2).count());
-        printf("\nLkFlow2 took %d ms, corner2 took %d ms", chrono::duration_cast<chrono::milliseconds>(endlk2 - endlk).count(), chrono::duration_cast<chrono::milliseconds>(endcorner2 - endcorner).count());
-        sumCornerTime += chrono::duration_cast<chrono::milliseconds>(endcorner2 - endinit).count();
-        sumLKTime += chrono::duration_cast<chrono::milliseconds>(endlk - endcorner2).count();
+        printf("\n>>>Found %d corners", corn.size());
+        printf("\n>>>Init took %ld ms, Corner detection took %ld ms, Lucas-Kanade took %ld ms", chrono::duration_cast<chrono::milliseconds>(endinit - begin).count(), chrono::duration_cast<chrono::milliseconds>(endcorner - endinit).count(), chrono::duration_cast<chrono::milliseconds>(endlk - endcorner).count());
+        sumCornerTime += chrono::duration_cast<chrono::milliseconds>(endcorner - endinit).count();
+        sumLKTime += chrono::duration_cast<chrono::milliseconds>(endlk - endcorner).count();
         sumTotalTime += chrono::duration_cast<chrono::milliseconds>(endlk - begin).count();
-        sumLK2Time += chrono::duration_cast<chrono::milliseconds>(endlk2 - endlk).count();
-
     }
 
     printf("\nAverage Corner Detection Time: %f", sumCornerTime / ((float)num_frames));
     printf("\nAverage Lucas-Kanade Time: %f", sumLKTime / ((float)num_frames));
     printf("\nAverage Total Time: %f", sumTotalTime / ((float)num_frames));
-    printf("\nAverage Lucas-Kanade 2 Time: %f", sumLK2Time / ((float)num_frames));
 }
