572bb91005aea1a289abb30b84224dbf999c28d5
La-Shallot <shallotwang129@gmail.com>
2024-05-04 23:21:17 -0400

    corner working???

--- build/run_perf.sh
+++ build/run_perf.sh
@@ -1,5 +1,10 @@
 make
-perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 1
+perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 1 > grep -v ">>>"
 perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 2
 perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 4
 perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 8
+perf stat -B -e cache-references,cache-misses ./Parellel_OpFlow_main -n 16
+# ./Parellel_OpFlow_main -n 1
+# ./Parellel_OpFlow_main -n 2
+# ./Parellel_OpFlow_main -n 4
+# ./Parellel_OpFlow_main -n 8
