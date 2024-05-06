2b2dedb2cb42df7a8199bc2eb3c25af4b844cd7c
La-Shallot <shallotwang129@gmail.com>
2024-05-06 02:24:54 -0400

    working

--- src/Harris.cpp
+++ src/Harris.cpp
@@ -14,69 +14,50 @@ using namespace std;
 
 #include <harris_corner.h>
 
-// hide the local functions in an anon namespace
-
 HarrisCorner::HarrisCorner() {}
 
-// Function to calculate gradients using Sobel operators
-inline void calculateGradients(const vector<vector<float>>& img, int x, int y, int xarea, int yarea, float& dx2, float& dy2, float& dxy) {
-    float dx, dy;
-    dx2 = dy2 = dxy = 0.0f;
-
-
-    int dimx = img[0].size(), dimy = img.size();
-
-    for (int j = -yarea; j <= yarea; ++j) {
-        for (int i = -xarea; i <= xarea; ++i) {
-            if (y + j > 0 && y + j < dimy - 1 && x + i > 0 && x + i < dimx - 1) {
-                dx = img[y + j][x + i + 1] - img[y + j][x + i - 1];
-                dy = img[y + j + 1][x + i] - img[y + j - 1][x + i];
-                dx2 += dx * dx;
-                dy2 += dy * dy;
-                dxy += dx * dy;
-            }
-        }
-    }
-}
-
-/**
- * Finds corners in an image using the Harris corner detection algorithm.
- *
- * @param img The input image represented as a 2D vector of floats.
- * @param xarea The size of the window in the x-direction for corner detection.
- * @param yarea The size of the window in the y-direction for corner detection.
- * @param thres The threshold value for corner detection.
- * @param verbose Flag indicating whether to print verbose output.
- * @return A vector of pairs representing the coordinates of the detected corners.
- */
-std::vector<std::pair<int, int>> HarrisCorner::findCorners(std::vector<std::vector<float>>& img, int xarea, int yarea, int thres, bool verbose){
+std::vector<std::pair<int, int>> HarrisCorner::findCorners(std::vector<std::vector<float>> &img, int xarea, int yarea, int thres, bool verbose)
+{
     const float k = 0.04f; // Harris constant
 
     std::vector<std::pair<int, int>> corners;
 
-
     int dimx = img[0].size(), dimy = img.size();
 
-
     #pragma omp parallel for schedule(static, 16)
-    for (int y = yarea; y < dimy - yarea; ++y) {
-        for (int x = xarea; x < dimx - xarea; ++x) {
-            float dx2, dy2, dxy;
-			
-            calculateGradients(img, x, y, xarea, yarea, dx2, dy2, dxy);
+    for (int y = yarea; y < dimy - yarea; ++y)
+    {
+        for (int x = xarea; x < dimx - xarea; ++x)
+        {
+            float dx2 = 0.0f, dy2 = 0.0f, dxy = 0.0f;
+            float dx = 0.0f, dy = 0.0f;
+
+            for (int j = -yarea; j <= yarea; ++j)
+            {
+                for (int i = -xarea; i <= xarea; ++i)
+                {
+                    if (y + j > 0 && y + j < dimy - 1 && x + i > 0 && x + i < dimx - 1)
+                    {
+                        dx = img[y + j][x + i + 1] - img[y + j][x + i - 1];
+                        dy = img[y + j + 1][x + i] - img[y + j - 1][x + i];
+                        dx2 += dx * dx;
+                        dy2 += dy * dy;
+                        dxy += dx * dy;
+                    }
+                }
+            }
 
             float det = dx2 * dy2 - dxy * dxy;
             float trace = dx2 + dy2;
             float response = det - k * trace * trace;
 
-            if (response > thres) {
+            if (response > thres)
+            {
                 #pragma omp critical
-                    corners.push_back(std::make_pair(x, y));
+                corners.push_back(std::make_pair(x, y));
             }
         }
     }
-	printf("I am here\n");
-	printf("Corners size: %d\n", corners.size());
 
     return corners;
 }
--- src/MorevacCorner.cpp
+++ src/MorevacCorner.cpp
@@ -7,169 +7,123 @@
 
 MorevacCorner::MorevacCorner() {}
 
-std::deque<cv::Point> MorevacCorner::findCorners(cv::Mat img, int xarea, int yarea, int thres, bool verbose = true, bool visual = false) {
+std::deque<cv::Point> MorevacCorner::findCorners(cv::Mat img, int xarea, int yarea, int thres, bool verbose = true, bool visual = false)
+{
     std::deque<cv::Point> corners;
 
-            cv::Mat outimg = img.clone(); // This will be used to provide a visual indication of the corners present
+    cv::Mat outimg = img.clone(); // This will be used to provide a visual indication of the corners present
 
-            int dimx = img.cols, dimy = img.rows;
-            // omp_lock_t vec_lock;
-            // omp_init_lock(&vec_lock);
+    int dimx = img.cols, dimy = img.rows;
 
-            int count = 0;
-            #pragma omp parallel for schedule(dynamic, 32)
-            // for (int startx_dummy = 0; (startx_dummy) < dimx/xarea - 1; startx_dummy += 1)
-            //     for (int starty = 0; (starty + yarea) < dimy; starty += yarea)
-            for (int starty_dummy = 0; (starty_dummy) < dimy/yarea - 1; starty_dummy += 1)
-                for (int startx = 0; (startx + xarea) < dimx; startx += xarea)
+    int count = 0;
+    #pragma omp parallel for schedule(dynamic, 32)
+    for (int starty_dummy = 0; (starty_dummy) < dimy / yarea - 1; starty_dummy += 1)
+        for (int startx = 0; (startx + xarea) < dimx; startx += xarea)
+        {
+            int starty = starty_dummy * yarea;
+            count++;
+            cv::Mat curarea = img(cv::Range(starty, std::min(starty + yarea, dimy)), cv::Range(startx, std::min(dimx, startx + xarea)));
+            double results[2] = {0, 0};
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int newsx = startx, newsy = starty;
+                // Check similarity in each direction
+                switch (dir)
                 {
-                    // int startx = startx_dummy * xarea;
-                    int starty = starty_dummy * yarea;
-                    count++;
-                    if (verbose){
-                        // printf("\n Area %d - Currenty looking at area (%d-%d,%d-%d)\n", count, startx, startx + xarea, starty, starty + yarea);
-                    }
-                    cv::Mat curarea = img(cv::Range(starty, std::min(starty + yarea, dimy)), cv::Range(startx, std::min(dimx, startx + xarea)));
-                    double results[2] = {0, 0};
-                    for (int dir = 0; dir < 4; dir++)
-                    {
-                        int newsx = startx, newsy = starty;
-                        // Check similarity in each direction
-                        switch (dir)
-                        {
-                        case 0: // left
-                            newsx -= xarea;
-                            newsx = std::max(newsx, 0);
-                            break;
-                        case 1: // top
-                            newsy -= yarea;
-                            newsy = std::max(newsy, 0);
-                            break;
-                        case 2: // right
-                            newsx += xarea;
-                            newsx = std::min(newsx, dimx);
-                            break;
-                        case 3: // down
-                            newsy += yarea;
-                            newsy = std::min(newsy, dimy);
-                            break;
-                        default:
-                            break;
-                        }
-                        //                    printf("Current direction is %d. New area is (%d-%d,%d-%d)\n",dir,newsx,min(dimx,newsx+xarea),newsy,min(newsy+yarea,dimy));
-
-                        cv::Mat newarea = img(cv::Range(newsy, std::min(newsy + yarea, dimy)), cv::Range(newsx, std::min(newsx + xarea, dimx)));
-
-                        if (newarea.cols != curarea.cols || newarea.rows != curarea.rows)
-                        {
-                            continue;
-                        }
-                        cv::Mat diff = abs(curarea - newarea);
-                        //print cv::mean(diff)
-                        // auto mean = cv::mean(diff).val;
-                        // printf("CV mean %d, %d, %d, %d\n", mean[0], mean[1], mean[2], mean[3]);
-                        // auto mean_mean = cv::mean(cv::mean(diff)).val;
-                        // printf("CV mean mean: %d, %d, %d, %d\n", mean_mean[0], mean_mean[1], mean_mean[2], mean_mean[3]);
-                        // auto mean_mean_final = cv::mean(cv::mean(diff))(0);
-                        // printf("CV mean mean final: %d\n", mean_mean_final);
-                        
-                        // printf("CV mean mean: %s\n", cv::mean(cv::mean(diff)));
-                        // printf("Type of cv::mean(diff) is %s\n", typeid(cv::mean(diff)).name());
-                        results[dir % 2] = cv::mean(cv::mean(diff))(0);
-                    }
-                    results[0] /= 2;
-                    results[1] /= 2;
-
-                    // thresholding
-                    if (results[0] >= thres && results[1] >= thres)
-                    {   
-                        //lock
-                        // omp_set_lock(&vec_lock);
-                        // printf("Pushing corner %d,%d\n", startx, starty);
-                        #pragma omp critical
-                        corners.push_back(cv::Point(startx, starty));
-                        // #pragma omp critical
-                        // rectangle(outimg, Point(startx, starty), Point(startx + xarea, starty + yarea), Scalar(0), 2);
-                        // omp_unset_lock(&vec_lock);
-                    }
-
-                    // log << startx << "," << starty << "," << results[0] << "," << results[1] << "\n";
+                case 0: // left
+                    newsx -= xarea;
+                    newsx = std::max(newsx, 0);
+                    break;
+                case 1: // top
+                    newsy -= yarea;
+                    newsy = std::max(newsy, 0);
+                    break;
+                case 2: // right
+                    newsx += xarea;
+                    newsx = std::min(newsx, dimx);
+                    break;
+                case 3: // down
+                    newsy += yarea;
+                    newsy = std::min(newsy, dimy);
+                    break;
+                default:
+                    break;
                 }
-            // omp_destroy_lock(&vec_lock);
 
-            return corners;
+                cv::Mat newarea = img(cv::Range(newsy, std::min(newsy + yarea, dimy)), cv::Range(newsx, std::min(newsx + xarea, dimx)));
+
+                if (newarea.cols != curarea.cols || newarea.rows != curarea.rows)
+                {
+                    continue;
+                }
+                cv::Mat diff = abs(curarea - newarea);
+                results[dir % 2] = cv::mean(cv::mean(diff))(0);
+            }
+            results[0] /= 2;
+            results[1] /= 2;
+
+            // thresholding
+            if (results[0] >= thres && results[1] >= thres)
+            {
+                #pragma omp critical
+                corners.push_back(cv::Point(startx, starty));
+            }
+        }
+
+    return corners;
 }
 
-std::vector<std::pair<int, int>> MorevacCorner::findCorners(std::vector<std::vector<float>>& img, int xarea, int yarea, int thres, bool verbose = true)
-    {
-        std::vector<std::pair<int, int>> corners;
+std::vector<std::pair<int, int>> MorevacCorner::findCorners(std::vector<std::vector<float>> &img, int xarea, int yarea, int thres, bool verbose = true)
+{
+    std::vector<std::pair<int, int>> corners;
 
+    int dimx = img[0].size(), dimy = img.size();
 
-        int dimx = img[0].size(), dimy = img.size();
+    std::vector<std::pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        std::vector<std::pair<int, int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+    #pragma omp parallel for schedule(static, 16)
+    for (int startx_dummy = 0; (startx_dummy) < dimx / xarea - 1; startx_dummy += 1)
+        for (int starty = 0; (starty + yarea) < dimy; starty += yarea)
+        {
+            int startx = startx_dummy * xarea;
 
-        #pragma omp parallel for schedule(static, 16)
-        for (int startx_dummy = 0; (startx_dummy) < dimx/xarea - 1; startx_dummy += 1)
-            for (int starty = 0; (starty + yarea) < dimy; starty += yarea)
-        // for (int starty_dummy = 0; (starty_dummy) < dimy/yarea - 1; starty_dummy += 1)
-        //     for (int startx = 0; (startx + xarea) < dimx; startx += xarea)
+            double results[4] = {0, 0, 0, 0};
+            // for all four directions calculate the change
+
+            for (int currdir = 0; currdir < 4; currdir++)
             {
-                int startx = startx_dummy * xarea;
-                // int starty = starty_dummy * yarea;
-                if (verbose){
-                    // printf("\n Area %d - Currenty looking at area (%d-%d,%d-%d)\n", count, startx, startx + xarea, starty, starty + yarea);
+                std::pair<int, int> offset = dir[currdir];
+                int newsx = startx + offset.first * xarea;
+                int newsy = starty + offset.second * yarea;
+
+                if (newsx < 0 || newsy < 0 || newsx + xarea >= dimx || newsy + yarea >= dimy)
+                {
+                    continue;
                 }
 
-                double results[4] = {0, 0, 0, 0};
-                //for all four directions calculate the change
-                
-                for (int currdir = 0; currdir < 4; currdir++){
-                    std::pair<int, int> offset= dir[currdir];
-                    int newsx = startx + offset.first * xarea;
-                    int newsy = starty + offset.second * yarea;
-                    
-                    if (newsx < 0 || newsy < 0 || newsx + xarea >= dimx || newsy + yarea >= dimy){
-                        if (verbose){
-                            //printf("Skipping due to boundary issues");
-                        }
-                        continue;
-                    }
-                    // printf("Current direction is %d. New area is (%d-%d,%d-%d)\n",dir,newsx,min(dimx,newsx+xarea),newsy,min(newsy+yarea,dimy));
-
-                    //get E value
-                    for (int j = 0; j < yarea; ++j) {
-                        for (int i = 0; i < xarea; ++i) {
-                            float oldPixel = img[starty + j][startx + i];
-                            float newPixel = img[newsy + j][newsx + i];
-                            float result = newPixel - oldPixel;
-                            results[currdir] += result*result;
-                        }
+                // get E value
+                for (int j = 0; j < yarea; ++j)
+                {
+                    for (int i = 0; i < xarea; ++i)
+                    {
+                        float oldPixel = img[starty + j][startx + i];
+                        float newPixel = img[newsy + j][newsx + i];
+                        float result = newPixel - oldPixel;
+                        results[currdir] += result * result;
                     }
                 }
-
-                //get the smallest value from the directions
-                float minval=std::min(results[0],std::min(results[1],std::min(results[2],results[3])));
-
-
-                // thresholding for the min value obtained per direction: local maxima means corner :)
-                if ( minval > thres)
-                {   
-                    //lock
-                    // omp_set_lock(&vec_lock);
-                    // printf("Pushing corner %d,%d\n", startx, starty);
-                    #pragma omp critical
-                    corners.push_back(std::make_pair(startx, starty));
-                    // #pragma omp critical
-                    // rectangle(outimg, Point(startx, starty), Point(startx + xarea, starty + yarea), Scalar(0), 2);
-                    // omp_unset_lock(&vec_lock);
-                }
-
-                // log << startx << "," << starty << "," << results[0] << "," << results[1] << "\n";
             }
-        // log.close();
 
-        // omp_destroy_lock(&vec_lock);
+            // get the smallest value from the directions
+            float minval = std::min(results[0], std::min(results[1], std::min(results[2], results[3])));
 
-        return corners;
-    }
+            // thresholding for the min value obtained per direction: local maxima means corner :)
+            if (minval > thres)
+            {
+                #pragma omp critical
+                corners.push_back(std::make_pair(startx, starty));
+            }
+        }
+
+    return corners;
+}
